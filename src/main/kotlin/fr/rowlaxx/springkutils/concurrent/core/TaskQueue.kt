d2d3ea2972c99976a2364496bd78df9d0146fe60
RowlaxX <theo.linder1@gmail.com>
2026-05-30 11:59:28 +0200

    WIP

--- src/main/kotlin/fr/rowlaxx/springkutils/concurrent/core/TaskQueue.kt
+++ src/main/kotlin/fr/rowlaxx/springkutils/concurrent/core/TaskQueue.kt
@@ -6,18 +6,44 @@ import kotlinx.coroutines.sync.withLock
 
 class TaskQueue(
     dispatcher: CoroutineDispatcher,
+    paused: Boolean = false
 ) {
     private val scope = CoroutineScope(dispatcher + SupervisorJob())
-    private val mutex = Mutex()
+    private val queueMutex = Mutex()
+    private val pauseMutex = Mutex(locked = paused)
     private var lastTask: Job? = null
 
+    var isPaused: Boolean = paused
+        private set
+
     fun enqueue(task: suspend () -> Unit) {
-        scope.launch { mutex.withLock {
-            lastTask = lastTask.let { last -> scope.launch {
-                last?.join()
-                task()
-            }}
+        scope.launch { queueMutex.withLock {
+            lastTask = scope.launch {
+                lastTask?.join()
+
+                pauseMutex.withLock {
+                    task()
+                }
+            }
         }}
+    }
+
+    fun pause() {
+        scope.launch { queueMutex.withLock {
+            if (!isPaused) {
+                pauseMutex.lock() // Lock the gate
+                isPaused = true
+            }
+        } }
+    }
+
+    fun resume() {
+        scope.launch { queueMutex.withLock {
+            if (isPaused) {
+                pauseMutex.unlock() // Open the gate
+                isPaused = false
+            }
+        } }
     }
 
     fun close() {
