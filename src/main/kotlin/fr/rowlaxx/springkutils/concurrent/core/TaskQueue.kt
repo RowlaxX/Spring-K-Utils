5910982ecb39bca1934a4c89226f51798dd12dd3
RowlaxX <theo.linder1@gmail.com>
2026-06-02 22:06:30 +0200

    WIP

--- src/main/kotlin/fr/rowlaxx/springkutils/concurrent/core/TaskQueue.kt
+++ src/main/kotlin/fr/rowlaxx/springkutils/concurrent/core/TaskQueue.kt
@@ -2,36 +2,68 @@ package fr.rowlaxx.springkutils.concurrent.core
 
 import fr.rowlaxx.springkutils.logging.utils.LoggerExtension.log
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 
 class TaskQueue(
     dispatcher: CoroutineDispatcher,
     paused: Boolean = false
 ) {
+    private class TaskHolder<T>(
+        val task: suspend () -> T,
+        val deferred: CompletableDeferred<T>
+    )
+
     private val scope = CoroutineScope(dispatcher + SupervisorJob())
-    private val queueMutex = Mutex()
-    private var lastTask: Job? = null
+    private val channel = Channel<TaskHolder<*>>(Channel.UNLIMITED)
 
     private val isPaused = MutableStateFlow(paused)
+    private val isClosed = MutableStateFlow(false)
+
+    private val loop: Job
+
+    init {
+        loop = scope.launch {
+            for (holder in channel) {
+                combine(isPaused, isClosed) { b1, b2 -> !b1 || b2 }.first { it }
+
+                if (isClosed.value && isPaused.value) {
+                    holder.deferred.completeExceptionally(CancellationException("TaskQueue closed while paused. Task aborted."))
+                    continue
+                }
+
+                if (!holder.deferred.isCancelled) {
+                    run(holder)
+                }
+            }
+        }
+    }
+
+    private suspend fun <T> run(holder: TaskHolder<T>) {
+        runCatching { holder.task() }
+            .onFailure {
+                if (it !is CancellationException) {
+                    log.error("An error has occurred in TaskQueue", it)
+                }
+            }
+            .let { holder.deferred.completeWith(it) }
+    }
 
     fun <T> enqueue(task: suspend () -> T): Deferred<T> {
         val deferred = CompletableDeferred<T>()
 
-        scope.launch { queueMutex.withLock {
-            val previous = lastTask
+        if (isClosed.value) {
+            deferred.completeExceptionally(CancellationException("TaskQueue closed"))
+            return deferred
+        }
 
-            lastTask = scope.launch {
-                previous?.join()
-                isPaused.first { !it }
+        val result = channel.trySend(TaskHolder(task, deferred))
 
-                runCatching { task() }
-                    .onFailure { log.error("An error has occurred in TaskQueue", it) }
-                    .let { deferred.completeWith(it) }
-            }
-        }}
+        if (result.isFailure) {
+            deferred.completeExceptionally(CancellationException("TaskQueue closed"))
+        }
 
         return deferred
     }
@@ -44,6 +76,12 @@ class TaskQueue(
     fun resume() { isPaused.value = false }
 
     fun close() {
-        scope.cancel()
+        if (isClosed.compareAndSet(expect = false, update = true)) {
+            channel.close()
+        }
+    }
+
+    suspend fun join() {
+        loop.join()
     }
 }
