b1fd2636f8d79a6a9c43470e81f610e80e21b135
RowlaxX <theo.linder1@gmail.com>
2026-08-04 12:46:44 +0200

    Update Coinw and Coinbase WebSocket shift duration to 20 hours; refactor continuity handling

--- src/main/kotlin/fr/rowlaxx/springkutils/concurrent/config/GlobalThreadConfiguration.kt
+++ src/main/kotlin/fr/rowlaxx/springkutils/concurrent/config/GlobalThreadConfiguration.kt
@@ -12,12 +12,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.task.TaskDecorator
 import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.RejectedExecutionException
-import java.util.concurrent.RejectedExecutionHandler
-import java.util.concurrent.ThreadFactory
-import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.*
 import java.util.concurrent.atomic.AtomicLong
 import kotlin.math.max
 
